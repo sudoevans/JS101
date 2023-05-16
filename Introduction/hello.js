71e885772879cc11d2660c68aa958b499988c8e1
Gamer Snave <snaveqiptoo@gmail.com>
2023-05-16 12:26:57 +0300

    Hello World

---
+++ Introduction/hello.js
@@ -0,0 +1,13 @@
+// Javascript Lesson
+
+// Introduction to Javascript
+
+// 1.0 Hello World!
+console.log("Hello World!");
+
+// 1.1 Variables
+
+
+// 1.2 Operators
+// +, -, *, /, %, ++, --, +=, -=, *=, /=, %=
+// ==, ===, !=, !==, >, <, >=, <=, &&, ||, !
