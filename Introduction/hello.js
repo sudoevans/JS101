273fc9435eb11008844206f1e987a62df5dda829
Gamer Snave <snaveqiptoo@gmail.com>
2023-05-17 11:37:05 +0300

    Variables in JS

--- Introduction/hello.js
+++ Introduction/hello.js
@@ -1,13 +1,1 @@
-// Javascript Lesson
-
-// Introduction to Javascript
-
-// 1.0 Hello World!
 console.log("Hello World!");
-
-// 1.1 Variables
-
-
-// 1.2 Operators
-// +, -, *, /, %, ++, --, +=, -=, *=, /=, %=
-// ==, ===, !=, !==, >, <, >=, <=, &&, ||, !
---
+++ Introduction/variables.js
@@ -0,0 +1,67 @@
+
+// ------VARIABLES-----
+// let car
+// let car ❌
+
+let car='Audi';
+// car='Dodge'; ✅
+
+var subject='Math'
+// var subject='Science'✅ 
+
+// console.log(car)
+console.log(subject)
+
+// const 
+const path="user/snave/documents";
+// path='home'; ❌
+
+console.log(path);
+
+// -----OPERATORS------
+// Arithmetic Operators
+// + - * / % ++ --
+
+let num1=10;
+let num2=20;
+// Sum
+sum=num1+num2;
+console.log('The sum is: '+sum);
+
+// Multiplication
+mult=num1*num2;
+console.log(mult);
+
+// Division
+div=num2/num1;
+console.log(div);
+
+// Modulus
+mod=num2%num1;
+console.log('Modulus the numbers is: '+mod);
+
+// Simple program for practice.
+// Write a program to calculate Area of a cirlce.
+
+// Area of a circle = pi * r * r
+CONST = 3.14 //const because we dont want the value of pi to change.
+
+let radius=10;
+let area=CONST*radius*radius;
+console.log('The area of the cirlce is: '+area);
+
+
+/*
+By the way, you can output more than one variable using a comma
+eg: console.log(sum,mod,area,div);✅
+*/
+
+// Quiz ❓
+// Write a program to find the area of a Sphere 
+/* Steps
+1. Declare a variable for radius
+2. Declare a constant for pi
+3. Declare a variable for area
+4. Calculate the area
+5. Output the area with console.log(xx)
+*/
