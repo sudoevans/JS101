ba972682efd2f018c267b7883170a6dee922ba09
Gamer Snave <snaveqiptoo@gmail.com>
2023-05-18 22:00:04 +0300

    conditionals

---
+++ Introduction/conditions.js
@@ -0,0 +1,17 @@
+// Assignment operators in Javascript.
+
+// if else in javascript
+let y=10,x=20;
+
+if (y<10){
+    console.log("y is greater than 10")
+}
+
+// if else in Js
+if (y < 10) {
+    console.log('Y is less than 10'
+    )
+}
+else {
+    console.log('Y is greater than 10')
+}
