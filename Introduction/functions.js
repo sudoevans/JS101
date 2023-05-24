fa01a2758a606ecd3211f4316ad9fcf54cb9b010
Gamer Snave <snaveqiptoo@gmail.com>
2023-05-24 10:50:32 +0300

    Functions in Js

---
+++ Introduction/functions.js
@@ -0,0 +1,47 @@
+function hello_world(){
+    console.log("Hello world");
+}
+
+//
+
+function sayHello(name){
+    console.log("Hi "+name);
+}
+
+sayHello("June")
+
+
+some_variable=sayHello("Snave");
+
+function sum(a,b,c){
+    return a+b+c;
+
+}
+
+
+let sum_three=sum(10,10,15);
+
+console.log(sum_three);
+
+
+
+
+
+function multi(x,y){
+    solution=x*y;
+    console.log(x+" Multiplied by "+y+" is equal to: ",solution)
+
+}
+
+multi(4,5)
+multi(6,55)
+multi(45,55)
+multi(5,45)
+
+
+// Write a function to find the area of a circle.
+
+
+
+
+
