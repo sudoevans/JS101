1d4e97e7cc7fce80c8283965030715623819598e
Gamer Snave <snaveqiptoo@gmail.com>
2023-05-30 21:48:42 +0300

    updateii

--- The DOM/index.js
+++ The DOM/index.js
@@ -1,7 +1,7 @@
 function calculate_area(side){
     side=document.getElementById("lenght").value;
     var area=side*side; //basic calculation of area of square.
-    // document.getElementById("result").textContent="Area: "+area;
-    alert(area);
+    document.getElementById("result").textContent="Area: "+area;
+
 }
     
